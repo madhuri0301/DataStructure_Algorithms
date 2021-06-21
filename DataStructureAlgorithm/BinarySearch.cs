0eecb40c4a8871db4cef4e89b78157f59ae3f65e
Madhuri <harshupatil0301@gmail>
2021-06-21 11:49:39 +0530

    [Madhuri]Add : Searching Name Using Binary Search

---
+++ DataStructureAlgorithm/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureAlgorithm
+{
+    public class BinarySearch
+    {
+		public static int binarySearch(String[] arr, String Name)
+		{
+			int a = 0, b = arr.Length - 1;
+			while (a <= b)
+			{
+				int m = a + (b - a) / 2;
+
+				int res = Name.CompareTo(arr[m]);
+
+				// Check if x is present at mid
+				if (res == 0)
+					return m;
+
+				// If x greater, ignore left side
+				if (res > 0)
+					a = m + 1;
+
+				// If x is smaller, ignore right side
+				else
+					b = m - 1;
+			}
+
+			return -1;
+		}
+
+
+	}
+}
--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -7,12 +7,32 @@ namespace DataStructureAlgorithm
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Data Structure Algorithm Problems");
+            Console.WriteLine("Enter Your Choice");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    int[] array = { 22, 85, 43, 54, 11, 32 }; //initializing Parameters 
+                    BubbleSort bs = new BubbleSort();
+                    bs.bubblesort(array);
+                    Console.WriteLine("Sorted array");
+                    bs.printsortedarray(array);
+                    break;
 
-            int []array = { 22, 85, 43, 54, 11, 32 }; //initializing Parameters 
-            BubbleSort bs = new BubbleSort();
-            bs.bubblesort(array);
-            Console.WriteLine("Sorted array");
-            bs.printsortedarray(array); 
+                // Passing a value to the array
+                case 2:
+                    String[] arr = { "Pratik", "Murtaza", "Sandesh", "Parth" };
+                    String Name = "Murtaza";
+                    int result = BinarySearch.binarySearch(arr, Name);
+
+                    // Checking the value is present or not
+                    if (result == -1)
+                        Console.WriteLine("Name is not Present");
+
+                    else
+                        Console.WriteLine("Name is Present at " + "Position " + result);
+                    break;
+            }
         }
     }
 }
